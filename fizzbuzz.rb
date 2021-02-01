d0ec5982f9888717334cc1808c9e439998ca408d
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-01 04:03:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-    puts "FizzBuzz"
+    "FizzBuzz"
+  elsif number % 5 == 0
+    "Buzz"
+  elsif number % 3 == 0
+    "Fizz"
 end
         
 
